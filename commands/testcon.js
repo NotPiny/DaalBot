a58542f6c3fb0fc388298670e59b02cf72c50f6d
pinyalt <>
2022-07-29 23:27:02 +0100

    📑CMD: Untracked

---
+++ commands/testcon.js
@@ -0,0 +1,34 @@
+module.exports = {
+    category: 'Other',
+    description: 'Runs the current console based command being tested',
+  
+    slash: true,
+    testOnly: true,
+
+        minArgs: 1,
+        maxArgs: 2,
+        expectedArgs: '<text> <txt>',
+        expectedArgsTypes: ['STRING', 'STRING'],
+
+        options: [
+            {
+              name: 'text',
+              description: `The text to send to the console`,
+              type: 'STRING',
+              required: true,
+            },
+            {
+              name: 'txt',
+              description: 'Secondary text field',
+              type: 'STRING',
+              required: false,
+            }
+        ],
+  
+    callback: (interaction) => {
+      const text = interaction.options.getString('text');
+      const txt = interaction.options.getString('txt');
+      console.log(`fetched data: ${text}, ${txt}`);
+      return 'Check console'
+    },
+  }
