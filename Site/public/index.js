8be4372749a044a594a722a8f9b821e79ab358cb
pinyalt <>
2022-11-20 18:08:18 +0000

    Updated site + test commit

--- Site/public/index.js
+++ Site/public/index.js
@@ -3,10 +3,39 @@ const ServerAmountElement = document.getElementById('serverAmount');
 const API = 'http://178.62.73.54:3000/api';
 
 if (ServerAmountElement) {
-    const serverAmount = fetch(`${API}/get/general/serverAmount`)
+    (async() => {
+        try {
+        const serverAmount = fetch(`${API}/get/general/serverAmount`)
         .then(res => res.json())
         .then((data) => {
             console.log(data);
             ServerAmountElement.innerText = data.serverAmount.toString();
         })
+    } catch {
+        console.log('Error: Could not fetch server amount');
+    }
+    })();
+    
+}
+
+const unknownServers = document.getElementById('unknownServers');
+
+if (unknownServers) {
+    unknownServers.addEventListener('click', () => {
+        (() => {
+            const serverAmount = fetch(`${API}/get/general/serverAmount`)
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data);
+                ServerAmountElement.innerText = data.serverAmount.toString();
+            })
+            .catch((err) => {
+                console.log('Error: Could not fetch server amount');
+                alert(`Error: Could not fetch server amount
+
+Details: 
+${err}`);
+            })
+    })();
+  })
 }
--- launch-extra.js
+++ launch-extra.js
@@ -1,7 +1,7 @@
 require('./legacy/launch.js'); 
 require('./logs.js');
 require('./daal.js'); 
-require('./localhost/launch');
+// require('./localhost/launch');
 require('./XP/message.js');
 require('./custom/launch.js');
 require('./automod/automod.js');
