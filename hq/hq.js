34e2435335ca01a05444891ed121b966f9bf8d6c
pinyalt <>
2022-12-13 19:34:39 +0000

    Set url to NotPiny/DaalBot if commit url not found

--- hq/hq.js
+++ hq/hq.js
@@ -55,7 +55,7 @@ client.on('messageCreate', msg => {
             const commitEmbed = new MessageEmbed()
                 .setTitle(`New commit by ${data[0]} to ${data[2]}`)
                 .setDescription(data[1])
-                .setURL(data[3])
+                .setURL(data[3].includes('https://github.com/NotPiny/DaalBot/commits') ? data[3] : 'https://github.com/NotPiny/DaalBot')
 
             msg.delete();
             daalbot.getChannel(msg.guild.id, '1052304271221198898').send({ embeds: [commitEmbed] });
