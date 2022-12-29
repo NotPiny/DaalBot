0a8250f21b92a2ab1b866659536fb0753366a8e9
pinyalt <>
2022-12-29 11:47:29 +0000

    Add XP system

--- XP/message.js
+++ XP/message.js
@@ -1,31 +1,20 @@
 const XPamount = 15;
 const client = require('../client.js');
+const daalbot = require('../daalbot.js');
 const fs = require('fs');
-const botPath = '..';
+const path = require('path');
+const { WOKCommands } = require('../config.json');
 
 client.on('messageCreate', msg => {
     if (msg.author.bot) return;
     if (msg.content.startsWith(['!', '$', '.'])) return;
-    try {
-        if (fs.existsSync(`${botPath}/db/xp/${msg.guild.id}`)) {
-            if (fs.existsSync(`${botPath}/db/xp/${msg.guild.id}/${msg.author.id}.xp`)) {
-                let xp = fs.readFileSync(`${botPath}/db/xp/${msg.guildId}/${msg.author.id}.xp`, 'utf8');
-                const oldXp = xp;
-                xp = parseInt(xp) + XPamount;
-                fs.writeFileSync(`${botPath}/db/xp/${msg.guildId}/${msg.author.id}.xp`, xp.toString());
-            } else {
-                let xp = "0";
-                const oldxp = fs.readFileSync(`${botPath}/db/xp/${msg.guildId}/${msg.author.id}.xp`, 'utf8');
-                xp = parseInt(xp) + XPamount;
-                fs.appendFileSync(`${botPath}/db/xp/${msg.guildId}/${msg.author.id}.xp`, xp.toString());
-            }
-        } else {
-            fs.mkdirSync(`${botPath}/db/xp/${msg.guild.id}`, { recursive: true });
-            let xp = "0";
-            xp = parseInt(xp) + XPamount;
-            fs.appendFileSync(`${botPath}/db/xp/${msg.guildId}/${msg.author.id}.xp`, xp.toString());
-        }
-} catch (err) {
-    return;
-}
+    
+    const GuildXpFolder = path.resolve(`./db/xp/${msg.guild.id}`);
+    const MemberXpFile = path.resolve(`./db/xp/${msg.guild.id}/${msg.author.id}.xp`);
+
+    if (!fs.existsSync(GuildXpFolder)) {
+        fs.mkdirSync(GuildXpFolder);
+    }
+
+    daalbot.fs.write(MemberXpFile, fs.existsSync(MemberXpFile) ? `${parseInt(fs.readFileSync(MemberXpFile)) + XPamount}` : `${XPamount}`);
 })
