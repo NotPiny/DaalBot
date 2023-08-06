463084eb6975e79fd52ba225ae74b15b2deefb2d
pinyalt <>
2023-08-06 13:02:22 +0100

    stuff :)

--- legacy/launch.js
+++ legacy/launch.js
@@ -1,15 +1,3 @@
-/**
-  template
-  
-  
-//const stuff 
-const client = require('../client');
-// Command Stuff :P
-client.on("messageCreate", msg => {
-    
-})
-*/
-
 require('./game.js');
 require('./IP.js');
 require('./olilz.js');
@@ -18,10 +6,6 @@ require('./delay.js');
 require('./quote.js');
 require('./mentionInfo.js');
 require('./status.js');
-// require('./vortex/vortex.js');
-// require('./stats.js');
 require('./confess-secret.js');
-require('./gmute.js')
 require('./DM.js');
 require('./ping-repeat.js');
-// require('./intRestart.js');
